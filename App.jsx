9198425ac0932a78b0920954219cad89bcccbd5f
Nikhileshkillada <113409646+Nikhileshkillada@users.noreply.github.com>
2022-12-02 23:37:23 -0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,53 @@
+/*
+import React, { Component } from 'react'
+import { Home } from './Components/Home'
+import { BrowserRouter, Route } from 'react-router-dom'
+
+
+
+export class App extends Component {
+    
+
+    render() {
+        return (
+            
+                    <BrowserRouter>
+                        
+                            {/* home }
+                            <Route exact path='/' component={() => <Home user={this.state.user} />} />
+                          
+                            
+                        
+                    </BrowserRouter>
+                
+        )
+    }
+}
+
+export default App;*/
+
+
+
+
+
+
+import './App.css';
+import { Home } from './Components/Home'
+import { BrowserRouter as Router} from 'react-router-dom'
+import { AddProducts } from './Components/AddProducts';
+import { ProductsContextProvider } from './global/ProductsContext';
+
+function App() {
+    return ( 
+        <ProductsContextProvider>
+            <Router>
+            <div className = "App">
+            <p> < Home /> </p>
+            <p> <AddProducts /> </p>
+            </div>
+            </Router>
+        </ProductsContextProvider>
+    );
+}
+
+export default App;
